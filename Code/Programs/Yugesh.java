39b471e09303c92b124f3acb512cf7adb38a04fa
Yugesh <81830361+Yugesh2247@users.noreply.github.com>
2022-08-19 08:17:21 +0530

    Update Yugesh.java

--- Code/Programs/Yugesh.java
+++ Code/Programs/Yugesh.java
@@ -15,6 +15,7 @@ public static void main(String[] args) {
                     System.out.print("  ");
                 }
              }
+                System.out.println();
             for(int j=1;j<=5;j++)
                {
                  System.out.print(" ");
